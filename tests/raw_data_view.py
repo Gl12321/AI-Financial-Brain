4bb53a8ad14d186407a2a6ca6374c7b237895f1b
Stanislav Rogov <stanislavrogov68@gmail.com>
2026-03-14 22:45:36 +0300

    chore: initialize project directories

---
+++ tests/raw_data_view.py
@@ -0,0 +1,22 @@
+from src.core.config import get_settings
+import pandas as pd
+
+settings = get_settings()
+
+movies_path = settings.DATA_RAW_DIR / "normalized_movies.csv"
+embeddings_path = settings.DATA_RAW_DIR / "movie_embeddings.csv"
+
+movies = pd.read_csv(movies_path)
+embeddings = pd.read_csv(embeddings_path)
+
+movies['tmdbId'] = movies['tmdbId'].astype(str)
+embeddings['tmdbId'] = embeddings['tmdbId'].astype(str)
+
+print(embeddings['title'][0], embeddings['embedding'][0])
+
+data = pd.merge(movies, embeddings, on="tmdbId", how="inner")
+
+print(f"Movies shape {movies.shape}")
+print(f"embeddings shape {embeddings.shape}")
+print(f"merged data shape {data.shape}")
+print(data.head())
