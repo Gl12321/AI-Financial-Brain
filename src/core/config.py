a8172740790a49031ec77da55d580b23255b05dc
Stanislav Rogov <stanislavrogov68@gmail.com>
2026-04-03 22:39:19 +0300

    chore: update config

--- src/core/config.py
+++ src/core/config.py
@@ -8,16 +8,39 @@ BASE_DIR = Path(__file__).resolve().parents[2]
 
 
 class Settings(BaseSettings):
-    GRAPH_HOST: str = Field(default="localhost")
-    GRAPH_PORT: int = Field(default=6379)
+    DB_HOST: str = "localhost"
+    DB_PORT: int = 5432
+    DB_USER: str = "admin"
+    DB_PASSWORD: str = "1234"
+    DB_NAME: str = "ai_finacial_bd"
+
+    NEO4J_HOST: str = "localhost"
+    NEO4J_PORT: int = 7687
+    NEO4J_USER: str = "neo4j"
+    NEO4J_PASSWORD: str = "12345678"
 
     DATA_RAW_DIR: Path = BASE_DIR / "data" / "raw"
     GRAPH_SEED_DIR: Path = BASE_DIR / "data" / "seed"
+    MODELS_DIR: Path = BASE_DIR / "models"
+
+    MODELS = {
+        "embedder": {
+            "repo_id": "thenlper/gte-large",
+            "dimension": 1024,
+            "device": "cpu",
+            "cache_path": MODELS_DIR / "embedder"
+        }
+    }
 
     model_config = SettingsConfigDict(
         env_file=str(BASE_DIR / ".env"),
+        env_file_encoding='utf-8',
         extra="ignore"
     )
+
+    @property
+    def db_url_async(self) -> str:
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
 
 @lru_cache()
